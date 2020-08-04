21920902bac9df759574b39fe6cae6c5aff4c47d
Jakub Slawinski <jakubslawinsky@gmail.com>
2020-08-04 11:33:32 +0200

    add button animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./scss/index.scss";
+
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", (event) => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach((link) => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
