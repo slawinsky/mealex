5e923c665070ed23d78bf734edd57607fda6c35d
Jakub Slawinski <jakubslawinsky@gmail.com>
2020-08-05 10:24:21 +0200

    add progress bars and counters animations

--- src/index.js
+++ src/index.js
@@ -18,3 +18,58 @@ window.addEventListener("scroll", (event) => {
     }
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+mainNavLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    mainNavLinks.forEach((link) => link.classList.remove("active"));
+    nav.classList.remove("nav--active");
+    hamburger.classList.remove("hamburger--active");
+    link.classList.toggle("active");
+  })
+);
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger--active");
+  nav.classList.toggle("nav--active");
+});
+
+const progressBars = document.querySelectorAll(".knowledge__bar span");
+const knowledge = document.querySelector(".knowledge");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY + innerHeight >= knowledge.offsetTop) {
+    progressBars.forEach((bar) => {
+      bar.style.width = `${bar.dataset.progress}%`;
+    });
+  }
+});
+
+const counters = document.querySelectorAll(".counter");
+const speed = 3000;
+
+const funFact = document.querySelector(".fun-fact");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY + innerHeight >= funFact.offsetTop) {
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+          counter.innerText = Math.ceil(count + inc);
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCount();
+    });
+  }
+});
